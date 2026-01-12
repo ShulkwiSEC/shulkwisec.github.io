62d22583b710e05e8274d19cb612f798e32257cd
shulkwisec <shulkwisec@windows.dev>
2026-01-12 22:34:19 +0300

    update deployment workflow and abs path on vite.config.github

--- vite.config.github.ts
+++ vite.config.github.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: '../dist/public',
+    outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
     target: 'esnext',
     rollupOptions: {
